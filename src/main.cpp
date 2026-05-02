94097c1abd4aecbe68548298404f1a01f50ad639
Richard Haar <rh@richhaar.com>
2026-05-02 10:56:53 -0400

    feat: Main updated with WorldBuilder

--- src/main.cpp
+++ src/main.cpp
@@ -4,58 +4,66 @@
 
 #include "rt/graphics/canvas.h"
 #include "rt/graphics/colour_rgb.h"
-#include "rt/graphics/lighting.h"
 #include "rt/graphics/ppm_writer.h"
+#include "rt/graphics/render.h"
 #include "rt/math/matrix_ops.h"
 #include "rt/math/point3.h"
 #include "rt/math/transforms.h"
+#include "rt/math/view_transform.h"
+#include "rt/world/camera.h"
 #include "rt/world/point_light.h"
 #include "rt/world/ray.h"
 #include "rt/world/sphere.h"
+#include "rt/world/world_builder.h"
 
 int main() {
-  auto constexpr width = 600;
-  auto constexpr height = 600;
-  auto canvas = rt::Canvas{width, height};
+  auto builder = rt::WorldBuilder();
+  auto constexpr pi = std::numbers::pi_v<float>;
 
-  auto sph = rt::Sphere{};
-  auto material = rt::Material{};
-  material.colour.green = 0.2f;
-  sph.SetMaterial(material);
-  // sph.SetTransform(rt::Shear(1, 0, 0, 0, 0, 0) *
-  // rt::Scaling(0.5f, 1.0f, 1.0f));
+  auto constexpr floor_material =
+      rt::Material{.colour = rt::ColourRGB{1.0f, 0.9f, 0.9f}, .specular = 0.0f};
+  auto const world =
+      builder
+          // Floor
+          .AddSphere(rt::Scaling(10.0f, 0.01f, 10.0f), floor_material)
+          // Left wall
+          .AddSphere(rt::Translation(0.0f, 0.0f, 5.0f) *
+                         rt::RotateY(-pi / 4.0f) * rt::RotateX(pi / 2.0f) *
+                         rt::Scaling(10.0f, 0.01f, 10.0f),
+                     floor_material)
+          // Right wall
+          .AddSphere(rt::Translation(0.0f, 0.0f, 5.0f) *
+                         rt::RotateY(pi / 4.0f) * rt::RotateX(pi / 2.0f) *
+                         rt::Scaling(10.0f, 0.01f, 10.0f),
+                     floor_material)
+          // Large green sphere
+          .AddSphere(rt::Translation(-0.5f, 1.0f, 0.5f),
+                     rt::Material{.colour = rt::ColourRGB{0.1f, 1.0f, 0.5f},
+                                  .diffuse = 0.7f,
+                                  .specular = 0.3f})
+          // Smaller green sphere
+          .AddSphere(rt::Translation(1.5f, 0.5f, -0.5f) *
+                         rt::Scaling(0.5f, 0.5f, 0.5f),
+                     rt::Material{.colour = rt::ColourRGB{0.5f, 1.0f, 0.5f},
+                                  .diffuse = 0.7f,
+                                  .specular = 0.3f})
+          // Smallest sphere
+          .AddSphere(rt::Translation(-1.5f, 0.33f, -0.75f) *
+                         rt::Scaling(0.33f, 0.33f, 0.33f),
+                     rt::Material{.colour = rt::ColourRGB{1.0f, 0.8f, 0.1f},
+                                  .diffuse = 0.7f,
+                                  .specular = 0.9f})
+          // Light source
+          .AddLight(rt::PointLight{rt::ColourRGB::White(),
+                                   rt::Point3{-10.0f, 10.0f, -10.0f}})
+          .Build();
 
-  auto constexpr light_position = rt::Point3{-10.0f, 10.0f, -10.0f};
-  auto constexpr light_colour = rt::ColourRGB::White();
-  auto const light = rt::PointLight(light_colour, light_position);
-
-  auto constexpr wall_size = 7.0f;
-  auto constexpr pixel_size_x = wall_size / width;
-  auto constexpr pixel_size_y = wall_size / height;
-
-  auto constexpr red = rt::ColourRGB{1.0f, 0.0f, 0.0f};
-
-  //-3.5 to +3.5
-  for (std::size_t y = 0; y < height; ++y) {
-    for (std::size_t x = 0; x < width; ++x) {
-      auto const direction =
-          Normalize(rt::Point3{-3.5f + pixel_size_x * x,
-                               -3.5f + pixel_size_y * y, 10.0f} -
-                    rt::Point3{0.0f, 0.0f, -5.0f});
-      auto const ray = rt::Ray{rt::Point3{0.0f, 0.0f, -5.0f}, direction};
-
-      if (auto const hit = sph.Hit(ray); hit.has_value()) {
-        auto const point = Position(ray, hit->first.t);
-        auto const eye = -ray.direction;
-
-        auto const colour = rt::Lighting(sph.GetMaterial(), light, point, eye,
-                                         sph.NormalAt(point));
-
-        canvas.Write(x, width - y, colour);
-      }
-    }
-  }
-  rt::PPMWriter::Write(canvas, "sphere_with_lighting.ppm");
-
+  auto const cam =
+      rt::Camera::Create(1000, 500, pi / 3.0f,
+                         rt::ViewTransform(rt::Point3{0.0f, 1.5f, -5.0f},
+                                           rt::Point3{0.0f, 1.0f, 0.0f},
+                                           rt::Vector3{0.0f, 1.0f, 0.0f}));
+  auto const img = rt::RenderWorld(cam, world);
+  rt::PPMWriter::Write(img, "default_world.ppm");
   return EXIT_SUCCESS;
 }

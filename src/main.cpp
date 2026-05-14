795e1df53edc32023cdce78fecf0bf8fb182d788
Richard Haar <rh@richhaar.com>
2026-05-14 14:27:26 -0400

    chore: clang-format main

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,6 @@ int main() {
   auto builder = rt::WorldBuilder();
   auto constexpr pi = std::numbers::pi_v<float>;
 
-
   auto checker_pattern = std::make_shared<rt::CheckerPattern>(
       rt::ColourRGB::Black(), rt::ColourRGB::White());
   checker_pattern->SetTransform(rt::Scaling(0.3f, 0.3f, 0.3f));
@@ -31,42 +30,66 @@ int main() {
   auto const green_sphere = rt::Material{
       .pattern = std::make_shared<rt::SolidColour>(0.2f, 0.8f, 0.1f)};
   auto const blue_sphere = rt::Material{
-    .pattern = std::make_shared<rt::SolidColour>(0.2f, 0.3f, 0.8f)};
+      .pattern = std::make_shared<rt::SolidColour>(0.2f, 0.3f, 0.8f)};
   auto const red_sphere = rt::Material{
-    .pattern = std::make_shared<rt::SolidColour>(1.0f, 0.3f, 0.1f)};
+      .pattern = std::make_shared<rt::SolidColour>(1.0f, 0.3f, 0.1f)};
   auto const yellow_sphere = rt::Material{
-    .pattern = std::make_shared<rt::SolidColour>(0.8f, 0.8f, 0.1f)};
+      .pattern = std::make_shared<rt::SolidColour>(0.8f, 0.8f, 0.1f)};
   auto const purple_sphere = rt::Material{
-    .pattern = std::make_shared<rt::SolidColour>(0.8f, 0.1f, 1.0f)};
+      .pattern = std::make_shared<rt::SolidColour>(0.8f, 0.1f, 1.0f)};
   auto const cyan_sphere = rt::Material{
-    .pattern = std::make_shared<rt::SolidColour>(0.0f, 0.8f, 0.9f)};
-  auto const reflective_sphere = rt::Material{
-    .ambient = 0.0f, .diffuse = 0.05f, .shininess = 300.0f, .reflective = 0.9f};
-  auto const refractive_sphere = rt::Material{
-    .ambient = 0.0f, .diffuse = 0.05f, .shininess = 300.0f, .reflective = 0.9f, .transparency = 0.9f, .refractive_index = 1.5f};
-  auto const checkered_floor = rt::Material{
-    .pattern = checker_pattern};
+      .pattern = std::make_shared<rt::SolidColour>(0.0f, 0.8f, 0.9f)};
+  auto const reflective_sphere = rt::Material{.ambient = 0.0f,
+                                              .diffuse = 0.05f,
+                                              .shininess = 300.0f,
+                                              .reflective = 0.9f};
+  auto const refractive_sphere = rt::Material{.ambient = 0.0f,
+                                              .diffuse = 0.05f,
+                                              .shininess = 300.0f,
+                                              .reflective = 0.9f,
+                                              .transparency = 0.9f,
+                                              .refractive_index = 1.5f};
+  auto const checkered_floor = rt::Material{.pattern = checker_pattern};
   auto const world =
       builder.AddSphere(rt::Scaling(10.0f, 10.0f, 10.0f), {})
-  .AddLight(rt::PointLight{rt::ColourRGB::Red(), rt::Point3(-4.0f, 3.0f, -4.0f)})
-  .AddLight(rt::PointLight{rt::ColourRGB::Green(), rt::Point3(4.0f, 3.0f, -4.0f)})
-  .AddLight(rt::PointLight{rt::ColourRGB::Blue(), rt::Point3(0.0f, 3.0f, -4.0f)})
-          .AddSphere(rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(0.0f, 1.0, 0.0f), green_sphere)
-  .AddSphere(rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(-4.0f, 1.0, 4.0f), red_sphere)
-  .AddSphere(rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(-2.0f, 1.0, 5.5f), blue_sphere)
-  .AddSphere(rt::Scaling(0.8f, 0.8f, 0.8f) * rt::Translation(2.0f, 1.0, 1.0f), purple_sphere)
-  .AddSphere(rt::Scaling(0.4f, 0.4f, 0.4f) * rt::Translation(-3.0f, 1.0, -0.5f), reflective_sphere)
-  .AddSphere(rt::Scaling(0.4f, 0.4f, 0.4f) * rt::Translation(1.0f, 1.0, -2.5f), refractive_sphere)
-  .AddSphere(rt::Scaling(0.6f, 0.6f, 0.6f) * rt::Translation(0.0f, 1.0, -6.0f), purple_sphere)
-  .AddSphere(rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(-5.5f, 1.0, -7.0f), cyan_sphere)
-  .AddPlane(rt::Matrix<4,4>::Identity(), checkered_floor)
-  .Build();
+          .AddLight(rt::PointLight{rt::ColourRGB::Red(),
+                                   rt::Point3(-4.0f, 3.0f, -4.0f)})
+          .AddLight(rt::PointLight{rt::ColourRGB::Green(),
+                                   rt::Point3(4.0f, 3.0f, -4.0f)})
+          .AddLight(rt::PointLight{rt::ColourRGB::Blue(),
+                                   rt::Point3(0.0f, 3.0f, -4.0f)})
+          .AddSphere(
+              rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(0.0f, 1.0, 0.0f),
+              green_sphere)
+          .AddSphere(
+              rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(-4.0f, 1.0, 4.0f),
+              red_sphere)
+          .AddSphere(
+              rt::Scaling(0.2f, 0.2f, 0.2f) * rt::Translation(-2.0f, 1.0, 5.5f),
+              blue_sphere)
+          .AddSphere(
+              rt::Scaling(0.8f, 0.8f, 0.8f) * rt::Translation(2.0f, 1.0, 1.0f),
+              purple_sphere)
+          .AddSphere(rt::Scaling(0.4f, 0.4f, 0.4f) *
+                         rt::Translation(-3.0f, 1.0, -0.5f),
+                     reflective_sphere)
+          .AddSphere(
+              rt::Scaling(0.4f, 0.4f, 0.4f) * rt::Translation(1.0f, 1.0, -2.5f),
+              refractive_sphere)
+          .AddSphere(
+              rt::Scaling(0.6f, 0.6f, 0.6f) * rt::Translation(0.0f, 1.0, -6.0f),
+              purple_sphere)
+          .AddSphere(rt::Scaling(0.2f, 0.2f, 0.2f) *
+                         rt::Translation(-5.5f, 1.0, -7.0f),
+                     cyan_sphere)
+          .AddPlane(rt::Matrix<4, 4>::Identity(), checkered_floor)
+          .Build();
 
   auto const cam =
-    rt::Camera::Create(kWidth, kHeight, pi / 2.0f,
-                       rt::ViewTransform(rt::Point3{0.0f, 0.5f, -3.0f},
-                                         rt::Point3{0.0f, 0.5f, 0.0f},
-                                         rt::Vector3{0.0f, 1.0f, 0.0f}));
+      rt::Camera::Create(kWidth, kHeight, pi / 2.0f,
+                         rt::ViewTransform(rt::Point3{0.0f, 0.5f, -3.0f},
+                                           rt::Point3{0.0f, 0.5f, 0.0f},
+                                           rt::Vector3{0.0f, 1.0f, 0.0f}));
 
   std::cout << "Starting render..." << std::endl;
   auto t0 = std::chrono::steady_clock::now();
